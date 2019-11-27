3c17966262e03670f4c3a916609b238bd1fd334b
Unknown <jangarretsen01@gmail.com>
2019-11-27 14:00:47 +0100

    made the table more specific

--- view/home/index.php
+++ view/home/index.php
@@ -1,8 +1,8 @@
 <h1>Planning</h1>
 <table>
     <tr>
-        <th>Naam</th>
-        <th>Rijder</th>
+        <th>Naam rijder</th>
+        <th>Naam dier</th>
         <th>Start tijd</th>
         <th>Eind tijd</th>
         <th>Planning ID</th>
@@ -11,7 +11,7 @@
     foreach ($data["planned"] as $planned) {
 
         echo '<tr><td>'.$planned["RiderName"].'</td><td>'.$planned["HorseName"].'</td><td>'.$planned["start_time"].'</td><td>'.$planned["end_time"].'</td>
-<td>'.$planned["id"].'</td><td><a href="edit"><i class="fas fa-pen"></i></a> <a href="delete"><i class="fas fa-trash"></i></a></td></tr>';
+<td>'.$planned["id"].'</td><td><a href="edit/'.$planned["id"].'"><i class="fas fa-pen"></i></a> <a href="delete/'.$planned["id"].'"><i class="fas fa-trash"></i></a></td></tr>';
 
 
 
